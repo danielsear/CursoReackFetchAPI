12453f3fc79d5baf74d85be15c710cbd0eb8c34d
Danielsear <danielandradeseara@hotmail.com>
2020-10-22 17:32:14 -0300

    dividend listaemClientes

---
+++ listagemClientes.js
@@ -0,0 +1,24 @@
+//console.log(fetch("http://localhost:4000/clientes"));
+// Template literals:  ``, utilizado para o navegar entender a junção de html e js
+// ${comando js}> usado para mandar comado js dentro do Template literals
+
+//console.log(conteudoLinha);
+const exibeCliente = (cpf, nome) => {
+  const linha = document.createElement("tr");
+
+  const conteudoLinha = ` <td>${cpf}</td>
+      <td>${nome}</td>`;
+
+  linha.innerHTML = conteudoLinha;
+  return linha;
+};
+
+const corpoTabela = document.querySelector("[data-conteudo-tabela]");
+// a propriedade date-... > é usada para manipular js dentro do html
+//console.log(corpoTabela);
+//appendChild= anexar um filho
+listarClientes().then((exibe) => {
+  exibe.forEach((indice) => {
+    corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome));
+  });
+});
