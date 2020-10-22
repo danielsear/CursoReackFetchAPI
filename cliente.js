69857aed038117f5d2f1c915836cdb5cd2efd862
Danielsear <danielandradeseara@hotmail.com>
2020-10-22 15:36:13 -0300

    incorporating a function to enter the data

--- cliente.js
+++ cliente.js
@@ -3,19 +3,30 @@ const infromacoesClientes = [
     cpf: 105402105,
     nome: "daniel",
   },
+  {
+    cpf: 10544502105,
+    nome: "sophia",
+  },
 ];
 
-const conteudoLinha = `<tr>
-<td>${infromacoesClientes[0].cpf}</td>
-<td>${infromacoesClientes[0].nome}</td>
-<td></td>
-</tr>;`;
 // template literos:  ``, utilizado para o navegar entender a junção de html e js
 // ${comando js}> usado para mandar comado js dentro do literos
 
 //console.log(conteudoLinha);
+const exibeCliente = (cpf, nome) => {
+  const linha = document.createElement("tr");
+
+  const conteudoLinha = ` <td>${cpf}</td>
+      <td>${nome}</td>`;
+
+  linha.innerHTML = conteudoLinha;
+  return linha;
+};
 
 const corpoTabela = document.querySelector("[data-conteudo-tabela]");
 // a propriedade date-... > é usada para manipular js dentro do html
 //console.log(corpoTabela);
-corpoTabela.innerHTML = conteudoLinha;
+//appendChild= anexar um filho
+infromacoesClientes.forEach((indice) => {
+  corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome));
+});
